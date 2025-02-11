d8521ca264eb29c7e5efdd202f578855416a6808
Hana Barochova <hana.barochova02@gmail.com>
2025-02-11 15:39:44 +0100

    Pročištění od nadbytečných komentářů a nepoužitých kousků kódu

--- script.js
+++ script.js
@@ -36,10 +36,10 @@ menuItems.forEach((item) => {
   });
 });
 
-// Switcher for light and dark mode
+// Přepínání light and dark mode
 const switcher = document.querySelector(".switch input");
 const themeIcon = document.querySelector(".switcher-description i");
-// Functions
+
 const darkMode = () => {
   themeIcon.classList.replace("fa-sun", "fa-moon");
 };
@@ -60,7 +60,8 @@ const switchTheme = (event) => {
 
 switcher.addEventListener("change", switchTheme);
 
-// Mouseenter a mouseleave nad obrázky zasedaček
+
+// Mouseenter a mouseleave nad obrázky zasedaček - objevující se popisek
 const allRooms = document.querySelector(".rooms");
 const oneRoom = allRooms.querySelectorAll(".room");
 const imagePar = allRooms.querySelectorAll(".room-par");
@@ -81,7 +82,7 @@ oneRoom.forEach((oneRoom) => {
   });
 });
 
-// Countdown znaků
+// Countdown znaků a obarvování v textarea
 const textarea = document.querySelector(".message");
 const pCount = document.querySelector(".text-countdown");
 const divCount = document.querySelector(".counter");
@@ -99,7 +100,7 @@ textarea.addEventListener("input", () => {
   }
 });
 
-// Výběr divu s třídou .radio-buttons a uvnitř něj divu .inline-rb
+// Výběr divu s třídou .radio-buttons a uvnitř něj divu .inline-rb - pro rozšíření klikatelného pole pro výběr
 const radioButtonsDiv = document.querySelector(".radio-buttons");
 const inlineRbDivs = radioButtonsDiv.querySelectorAll(".inline-rb");
 
@@ -114,10 +115,9 @@ inlineRbDivs.forEach(div => {
   });
 });
 
-// Scroll icon visibility
+// Scrollování a viditelnost ikony
 const scrollIcon = document.querySelector(".scroll-up i")
 const scrollBox = document.querySelector(".scroll-up")
-
 
 window.addEventListener("scroll", () => {
   const computedScrollHeight = document.documentElement.scrollHeight - window.innerHeight;
@@ -131,7 +131,7 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// Funkce pro scrollování nahoru
+// Funkce pro scrollování nahoru do nuly
 scrollBox.addEventListener("click", () => {
   window.scrollTo({
     top: 0,
@@ -139,40 +139,36 @@ scrollBox.addEventListener("click", () => {
   });
 });
 
-// ===========================================================
-
 // Konstanty potřebné pro validaci formuláře a aktivaci buttonu submit
 const formular = document.querySelector("form");
 const email1 = formular.querySelector(".mail1");
 const email2 = formular.querySelector(".mail2");
 const submitBtn = formular.querySelector(".submit-btn");
-
-const dateFilled = document.getElementById("date");
-const timeFilled = document.getElementById("time");
 const mailDifferent = formular.querySelector(".mail-different");
 const thanksMessage = formular.querySelector(".thanks");
 const requiredFields = formular.querySelectorAll("input[required]");
 const requiredRadioFields = formular.querySelectorAll(".selected-room[required]");
 
-// Inicializace stavu tlačítka submit
+// Výchozí stav tlačítka submit nastaven na disabled
 submitBtn.disabled = true;
 
 // Funkce pro kontrolu shody e-mailů a aktivaci tlačítka
 function validateForm() {
   const allFieldsFilled = Array.from(requiredFields).every(field => field.value.trim() !== "");
 
+  // kontrola shody hodnot v poli email1 a email2 v reálném čase
   const emailsMatch = email1.value === email2.value;
   if (email2.value && !emailsMatch) {
     mailDifferent.style.display = "block";
   } else {
     mailDifferent.style.display = "none";
   }
-
+  // Kontrola zaškrtnutého 1 radiobuttonu
   const oneRadioChecked = Array.from(requiredRadioFields).some(
     (radio) => radio.checked,
   );
 
-  // Aktivace tlačítka pouze pokud jsou splněny podmínky
+  // Aktivace tlačítka pouze pokud jsou splněny podmínky a barevné ohraničení při splněných či nesplněných podmínkách pro odeslání
   submitBtn.disabled = !(allFieldsFilled && emailsMatch && oneRadioChecked);
 
   if (submitBtn.disabled) {
@@ -188,21 +184,18 @@ email2.addEventListener("input", validateForm);
 requiredFields.forEach(field => field.addEventListener("input", validateForm));
 requiredRadioFields.forEach(radio => radio.addEventListener("change", validateForm));
 
-
-// ===============================================================
-
-  // Akce po odeslání formuláře
-  document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault(); // Zamezení výchozímu odeslání formuláře
-    submitBtn.style.maxWidth = "250px";
-    submitBtn.value = "Odeslat další žádost";
-    submitBtn.style.color = getComputedStyle(document.documentElement).getPropertyValue("--button-color");
-    thanksMessage.style.display = "block";
-    formular.style.boxShadow = "none";
-    formular.style.borderRadius = "none";
+// Akce po odeslání formuláře
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault(); // Zamezení výchozímu odeslání formuláře
+  submitBtn.style.maxWidth = "250px";
+  submitBtn.value = "Odeslat další žádost";
+  submitBtn.style.color = getComputedStyle(document.documentElement).getPropertyValue("--button-color");
+  thanksMessage.style.display = "block";
+  formular.style.boxShadow = "none";
+  formular.style.borderRadius = "none";
   });
 
-  // Reload stránky po kliknutí na tlačítko "Odeslat další žádost"
+// Reload stránky po kliknutí na tlačítko "Odeslat další žádost"
   submitBtn.addEventListener("click", () => {
     if (submitBtn.value === "Odeslat další žádost") {
         location.reload();
